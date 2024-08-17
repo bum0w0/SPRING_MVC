245ee3f58cb70d54ba2f1d87d43d0bbaf2b6c953
kim-jin-bum <kgy5988@naver.com>
2024-08-18 02:40:30 +0900

    feat: 어댑터 패턴 도입
    핸들러 매핑에 ControllerV4를 사용하는 컨트롤러 추가, 해당 컨트롤러를 처리할 어댑터 ControllerV4HandlerAdapter 추가

---
+++ src/main/java/hello/servlet/web/frontcontroler/v5/adapter/ControllerV4HandlerAdapter.java
@@ -0,0 +1,44 @@
+package hello.servlet.web.frontcontroler.v5.adapter;
+
+import hello.servlet.web.frontcontroler.ModelView;
+import hello.servlet.web.frontcontroler.v4.ControllerV4;
+import hello.servlet.web.frontcontroler.v5.MyHandlerAdapter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ControllerV4HandlerAdapter implements MyHandlerAdapter {
+    @Override
+    public boolean supports(Object handler) {
+        return (handler instanceof ControllerV4);
+    }
+
+    @Override
+    public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException {
+        ControllerV4 controller = (ControllerV4) handler;
+
+        Map<String, String> paramMap = createParamMap(request);
+        HashMap<String, Object> model = new HashMap<>();
+
+        String viewName = controller.process(paramMap, model);
+
+        // 어댑터 변환
+        // 어댑터가 호출하는 ControllerV4 는 뷰의 이름을 반환한다. 그런데 어댑터는 뷰의 이름이 아니라 ModelView를 만들어서 반환해야 한다. 여기서 어댑터가 꼭 필요한 이유가 나온다.
+        // ControllerV4 는 뷰의 이름을 반환했지만, 어댑터는 이것을 ModelView로 만들어서 형식을 맞추어 반환한다.
+        ModelView mv = new ModelView(viewName);
+        mv.setModel(model);
+
+        return mv;
+    }
+
+    private static Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+        return paramMap;
+    }
+}
